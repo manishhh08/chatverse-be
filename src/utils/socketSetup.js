ffb9872b809bc3bb2d0e2accc78ed0d8ac0f6b3a
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-11-08 17:44:36 +1100

    socket setup fix

--- src/utils/socketSetup.js
+++ src/utils/socketSetup.js
@@ -1,9 +1,32 @@
 import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
 
 let io;
 
 export const initSocket = (server) => {
-  io = new Server(server, { cors: { origin: "*" } });
+  io = new Server(server, {
+    cors: {
+      origin: "https://chatverse-blush.vercel.app",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+    transports: ["websocket", "polling"],
+  });
+
+  // Socket authentication middleware
+  io.use((socket, next) => {
+    const token = socket.handshake.auth?.token; //
+    if (!token) return next(new Error("jwt must be provided"));
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      socket.userId = decoded._id; // attach user ID to socket
+      next();
+    } catch (err) {
+      next(new Error("Invalid token"));
+    }
+  });
+
   return io;
 };
 
